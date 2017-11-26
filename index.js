e97bb1dc2426e031180ae3164a94d5d9af91b64b
awbridgers <awbridgers@users.noreply.github.com>
2017-11-26 14:45:28 -0500

    update to generalize bot and add comments

--- index.js
+++ index.js
@@ -3,37 +3,37 @@ let config = require('./config.js');
 let cron = require('node-cron');
 let moment = require('moment-timezone')
 
-const T = new Twit(config);
+const T = new Twit(config);       //setup Twit with twitter API keys
 
-let tweetArray = new Set();
-let idArray = [];
 
+//use cron to schedule bot to check tweets every hour
 let job = cron.schedule('0 0-23 * * *', ()=>{
 
 
-T.get('search/tweets', {q: 'receive wake forest offer', count: 100, result_type: "recent"}, (err, data,response) => {
+T.get('search/tweets', {q: 'Your search query here', count: 100, result_type: "recent"}, (err, data,response) => {
   if (!err){
     let tweetArray = new Set();
     let idArray = [];
 
+    //loop through the array of tweets returned by twitter and filter out manual RT's and possibly NSFW material
     data.statuses.map((x)=> {
       if(x.text.substring(0,2) != "RT" && x.possibly_sensitive === false){
-        tweetArray.add(x);
-        //console.log(tweetArray.size)
+        tweetArray.add(x);      //add tweets to the set (using set to avoid adding duplicate tweets)
       }
     });
 
     let tweets = [];
-    tweets = Array.from(tweetArray);
+    tweets = Array.from(tweetArray);    //convert the set to an array to make use of map function
+    //create a new date object and format it for testing
     let d = new(Date);
     let myTimezone = "America/Toronto";
     let myDatetimeFormat= "YYYY-MM-DD hh:mm:ss a z";
     let date = moment(d).tz(myTimezone).format(myDatetimeFormat);
 
-    console.log(date);
+    console.log(date);    //log the date and time to quickly determine if the bot it running every hour
     console.log("Attempting to Retweet " + tweets.length + " tweets...");
-    tweets.map((x)=>console.log(x.text));
-    tweets.map((x)=> retweet(x.id_str));
+    tweets.map((x)=>console.log(x.text));   //log the tweet text just for testing
+    tweets.map((x)=> retweet(x.id_str));    //retweet the tweets in the array
 
 
   }
@@ -41,11 +41,12 @@ T.get('search/tweets', {q: 'receive wake forest offer', count: 100, result_type:
 
 
   else{
-    console.log(err);
+    console.log(err);   //log any errors
   }
 });
 }, null,true);
 
+//function to retweet tweets
 let retweet = (id) => T.post('statuses/retweet/:id', {id: id}, (err, response) => {
   if (err){
     console.log( "failed to Retweet");

f11b266c796a7c897f53c8a32212491be6a0930f
awbridgers <awbridgers@users.noreply.github.com>
2018-06-19 10:09:08 -0400

    updated to not include retweets or replies in the search results

---
+++ blockedTweets.js
@@ -0,0 +1,5 @@
+let blockedTweets = [
+  "1004850364024348672"
+]
+
+module.export = blockedTweets;
--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 let Twit = require('twit');
 let moment = require('moment-timezone')
+let includes = require('array-includes')
+
 
 require('dotenv').config();
 
@@ -14,24 +16,26 @@ const APP_CONFIG = {
 const T = new Twit(APP_CONFIG);       //setup Twit with twitter API keys
 
 
+//search and retweet scholarship offers
 
-T.get('search/tweets', {q: 'wake forest offer receive OR received', count: 100, result_type: "recent"}, (err, data,response) => {
+T.get('search/tweets', {q: 'wake forest offer receive OR received exclude:retweets exclude:replies', count: 100, result_type: "recent"}, (err, data,response) => {
   if (!err){
     let tweetArray = new Set();
     let idArray = [];
 
     //loop through the array of tweets returned by twitter and filter out manual RT's and possibly NSFW material
     data.statuses.map((x)=> {
-      if(x.text.substring(0,2) != "RT"){        //remove retweets from the list
-        if(!x.hasOwnProperty("possibly_sensitive")){        //if it doesn't have a link, attempt to retweet.
-        tweetArray.add(x);
-        //console.log(tweetArray.size)
-      }
-      else if(x.hasOwnProperty("possibly_sensitive") && x.possibly_sensitive===false){    //if it containst link, make sure its safe
-        tweetArray.add(x)
-      }
-  }
-    });
+      if(!includes(blockedTweets, x.id_str)){
+        if(x.text.substring(0,2) != "RT"){        //remove retweets from the list
+          if(!x.hasOwnProperty("possibly_sensitive")){        //if it doesn't have a link, attempt to retweet.
+          tweetArray.add(x);
+          //console.log(tweetArray.size)
+          }
+          else if(x.hasOwnProperty("possibly_sensitive") && x.possibly_sensitive===false){    //if it containst link, make sure its safe
+            tweetArray.add(x)
+          }
+        }
+      }});
 
     let tweets = [];
     tweets = Array.from(tweetArray);    //convert the set to an array to make use of map function
@@ -45,11 +49,53 @@ T.get('search/tweets', {q: 'wake forest offer receive OR received', count: 100, 
     console.log("Attempting to Retweet " + tweets.length + " tweets...");
     tweets.map((x)=>console.log(x.text));   //log the tweet text just for testing
     tweets.map((x)=> retweet(x.id_str));    //retweet the tweets in the array
-
-
   }
 
+  else{
+    console.log(err);   //log any errors
+  }
+});
 
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+//search and rewtweet commits
+T.get('search/tweets', {q: 'wake forest commit OR committed exclude:retweets exclude:replies', count: 100, result_type: "recent"}, (err, data,response) => {
+  if (!err){
+    let tweetArray = new Set();
+    let idArray = [];
+    data.statuses.map((x)=>console.log(x.text))
+
+    //loop through the array of tweets returned by twitter and filter out manual RT's and possibly NSFW material
+    data.statuses.map((x)=> {
+      if(!includes(blockedTweets, x.id_str)){
+        if(x.text.substring(0,2) != "RT"){        //remove retweets from the list
+          if(x.in_reply_to_status_id === null){     //remove replies
+            if(!x.hasOwnProperty("possibly_sensitive")){        //if it doesn't have a link, attempt to retweet.
+            tweetArray.add(x);
+            //console.log(tweetArray.size)
+            }
+            else if(x.hasOwnProperty("possibly_sensitive") && x.possibly_sensitive===false){    //if it containst link, make sure its safe
+              tweetArray.add(x)
+            }
+          }}
+        }});
+
+    let tweets = [];
+    tweets = Array.from(tweetArray);    //convert the set to an array to make use of map function
+    //create a new date object and format it for testing
+    let d = new(Date);
+    let myTimezone = "America/Toronto";
+    let myDatetimeFormat= "YYYY-MM-DD hh:mm:ss a z";
+    let date = moment(d).tz(myTimezone).format(myDatetimeFormat);
+
+    console.log(date);    //log the date and time to quickly determine if the bot is running every hour
+    console.log("Attempting to Retweet " + tweets.length + " tweets...");
+    //console.log("testing blocker " + blockedTweets[0])
+    tweets.map((x)=>console.log(x.text, x.id_str));   //log the tweet text just for testing
+    tweets.map((x)=> retweet(x.id_str));    //retweet the tweets in the array
+  }
 
   else{
     console.log(err);   //log any errors
@@ -58,10 +104,15 @@ T.get('search/tweets', {q: 'wake forest offer receive OR received', count: 100, 
 
 //function to retweet tweets
 let retweet = (id) => T.post('statuses/retweet/:id', {id: id}, (err, response) => {
-  if (err){
-    console.log( "failed to Retweet");
-  }
-  else{
-    console.log("Retweet successful");
-  }
-});
+    if (err){
+      console.log( "failed to Retweet");
+    }
+    else{
+      console.log("Retweet successful");
+    }
+  });
+
+
+let blockedTweets = [
+  "1004850364024348672"
+]
